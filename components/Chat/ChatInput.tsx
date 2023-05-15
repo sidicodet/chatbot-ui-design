7b6cd8dd19a65e3d752315cd53b19a5f41230a14
sidicodet <121307234+sidicodet@users.noreply.github.com>
2023-05-15 15:58:25 +0100

    ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -386,11 +386,11 @@ export const ChatInput = ({
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Imoogle AI & UI
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Imoogle UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
         )}
       </div>
     </div>
